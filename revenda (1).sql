c95fe546204ac0f2d28d3c658a60aa0af143c3da
jaydder <52470346+jaydder@users.noreply.github.com>
2023-09-22 20:36:38 -0300

    Initial Commit

---
+++ revenda (1).sql
@@ -0,0 +1,185 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Tempo de geração: 02-Set-2023 às 03:08
+-- Versão do servidor: 10.4.28-MariaDB
+-- versão do PHP: 8.0.28
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Banco de dados: `revenda`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `cliente`
+--
+
+CREATE TABLE `cliente` (
+  `id` int(11) NOT NULL,
+  `nome` varchar(60) NOT NULL,
+  `cpf` varchar(18) NOT NULL,
+  `data_inicio` date NOT NULL,
+  `data_fim` date NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura stand-in para vista `descontoview`
+-- (Veja abaixo para a view atual)
+--
+CREATE TABLE `descontoview` (
+`valorDesconto` decimal(10,2)
+);
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `veiculo`
+--
+
+CREATE TABLE `veiculo` (
+  `id` int(11) NOT NULL,
+  `marca` varchar(255) NOT NULL,
+  `nome` varchar(255) NOT NULL,
+  `modelo` varchar(255) NOT NULL,
+  `ano` int(11) NOT NULL,
+  `cor` varchar(25) NOT NULL,
+  `preco` decimal(10,2) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `venda`
+--
+
+CREATE TABLE `venda` (
+  `id` int(11) NOT NULL,
+  `valorDaVenda` decimal(10,2) NOT NULL,
+  `valorDesconto` decimal(10,2) NOT NULL,
+  `id_veiculo` int(11) NOT NULL,
+  `id_cliente` int(11) NOT NULL,
+  `id_vendedor` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Acionadores `venda`
+--
+DELIMITER $$
+CREATE TRIGGER `calcular_comissao` AFTER INSERT ON `venda` FOR EACH ROW BEGIN
+    DECLARE comissao DECIMAL(10, 2);
+    
+    -- Calcular a comissão (por exemplo, 10% do valor da venda)
+    SET comissao = NEW.valorDaVenda * 0.005;
+    
+    -- Inserir a comissão na tabela de comissões
+    INSERT INTO comissoes (venda_id, valor_comissao) VALUES (NEW.id, comissao);
+END
+$$
+DELIMITER ;
+DELIMITER $$
+CREATE TRIGGER `tr_vendadesconto` BEFORE INSERT ON `venda` FOR EACH ROW set new.valorDesconto = (NEW.valorDaVenda * 0.05)
+$$
+DELIMITER ;
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `vendedor`
+--
+
+CREATE TABLE `vendedor` (
+  `id` int(11) NOT NULL,
+  `nome` varchar(255) NOT NULL,
+  `cpf` varchar(15) NOT NULL,
+  `data_inicio` date NOT NULL,
+  `data_fim` date DEFAULT NULL,
+  `comissao` decimal(10,2) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura para vista `descontoview`
+--
+DROP TABLE IF EXISTS `descontoview`;
+
+CREATE ALGORITHM=UNDEFINED DEFINER=`root`@`localhost` SQL SECURITY DEFINER VIEW `descontoview`  AS SELECT `venda`.`valorDesconto` AS `valorDesconto` FROM `venda` ;
+
+--
+-- Índices para tabelas despejadas
+--
+
+--
+-- Índices para tabela `cliente`
+--
+ALTER TABLE `cliente`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Índices para tabela `veiculo`
+--
+ALTER TABLE `veiculo`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Índices para tabela `venda`
+--
+ALTER TABLE `venda`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `fk_venda_cliente` (`id_cliente`),
+  ADD KEY `fk_venda_vendedor` (`id_vendedor`),
+  ADD KEY `fk_venda_veiculo` (`id_veiculo`);
+
+--
+-- Índices para tabela `vendedor`
+--
+ALTER TABLE `vendedor`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT de tabelas despejadas
+--
+
+--
+-- AUTO_INCREMENT de tabela `cliente`
+--
+ALTER TABLE `cliente`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de tabela `venda`
+--
+ALTER TABLE `venda`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- Restrições para despejos de tabelas
+--
+
+--
+-- Limitadores para a tabela `venda`
+--
+ALTER TABLE `venda`
+  ADD CONSTRAINT `fk_venda_cliente` FOREIGN KEY (`id_cliente`) REFERENCES `cliente` (`id`),
+  ADD CONSTRAINT `fk_venda_veiculo` FOREIGN KEY (`id_veiculo`) REFERENCES `veiculo` (`id`),
+  ADD CONSTRAINT `fk_venda_vendedor` FOREIGN KEY (`id_vendedor`) REFERENCES `vendedor` (`id`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
